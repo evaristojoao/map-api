f865d0b7be39eb8617be9c131539fe2e332c54fd
João Eugénio <joaoeugenio20@gmail.com>
2026-02-03 13:48:37 +0000

    Implement post endpoint with django

--- books/views.py
+++ books/views.py
@@ -1,8 +1,21 @@
+import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from books.models import Book
 
 
+@csrf_exempt
 def book_view(request):
-    books = Book.objects.all() 
-    data = [{ 'id': book.id, 'name': book.name} for book in books] 
-    return JsonResponse(data, safe=False) 
+    if request.method == 'GET':
+        books = Book.objects.all()
+        data = [{'id': book.id, 'name': book.name} for book in books]
+
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        # padrão caracteres para não quebrar a string inputada
+        data = json.loads(request.body.decode('utf-8'))
+
+        new_book = Book(name=data['name'])
+        new_book.save()
+
+        return JsonResponse({'id': new_book.id, 'name': new_book.name}, status=201)
