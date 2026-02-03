0a42e7afed0cc3292083829af5619f0042f744f0
João Eugénio <joaoeugenio20@gmail.com>
2026-02-03 17:28:18 +0000

    Implement id get endpoint with django

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from books.views import book_view
+from books.views import book_create_list_view, book_detail_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', book_view, name='book-list'),
+    path('books/', book_create_list_view, name='book-list'),
+    path('books/<int:pk>/', book_detail_view, name='book-detail-view'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,21 +1,32 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from books.models import Book
 
 
-@csrf_exempt
-def book_view(request):
+@csrf_exempt 
+def book_create_list_view(request):
     if request.method == 'GET':
         books = Book.objects.all()
         data = [{'id': book.id, 'name': book.name} for book in books]
 
         return JsonResponse(data, safe=False)
     elif request.method == 'POST':
-        # padrão caracteres para não quebrar a string inputada
         data = json.loads(request.body.decode('utf-8'))
 
         new_book = Book(name=data['name'])
         new_book.save()
 
         return JsonResponse({'id': new_book.id, 'name': new_book.name}, status=201)
+    
+    
+@csrf_exempt 
+def book_detail_view(request, pk):
+    if request.method == 'GET':
+        book = get_object_or_404(Book, pk=pk)
+        data = {'id': book.id, 'name': book.name}
+
+        return JsonResponse(data)
+
+
