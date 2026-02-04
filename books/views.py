a5ba880ba5ddde4b4497d1234d3810cdeb5638d1
João Eugénio <joaoeugenio20@gmail.com>
2026-02-04 09:21:53 +0000

    Refactor `GET` and `POST` endpoint with DRF

--- books/views.py
+++ books/views.py
@@ -3,23 +3,14 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from books.models import Book
+from rest_framework import generics
+from books.serializers import BookSerializer
 
 
-@csrf_exempt 
-def book_create_list_view(request):
-    if request.method == 'GET':
-        books = Book.objects.all()
-        data = [{'id': book.id, 'name': book.name} for book in books]
-
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-
-        new_book = Book(name=data['name'])
-        new_book.save()
-
-        return JsonResponse({'id': new_book.id, 'name': new_book.name}, status=201)
-
+class BookCreateListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
 
 @csrf_exempt 
 def book_detail_view(request, pk):
