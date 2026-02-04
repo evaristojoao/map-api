dd1697f6815f45b98b3c991ab46913fec732930b
João Eugénio <joaoeugenio20@gmail.com>
2026-02-04 15:46:21 +0000

    Add new app `categories`

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from books.views import BookCreateListView, BookRetrieveUpdateDestroyView
+from categories.views import CategoryCreateListView, CategoryRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', BookCreateListView.as_view(), name='book-list'),
+    path('books/', BookCreateListView.as_view(), name='book-create-list'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail-view'),
+    path('categories/', CategoryCreateListView.as_view(), name='category-create-list'),
+    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail-view'),
 ]
