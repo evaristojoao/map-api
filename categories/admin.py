c6e751c54941f7ad744fa4349cb53aab5ac19699
João Eugénio <joaoeugenio20@gmail.com>
2026-02-05 17:34:07 +0000

    Add `description` parameter to app categories `admin.py`

--- categories/admin.py
+++ categories/admin.py
@@ -4,4 +4,4 @@ from categories.models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'description')
