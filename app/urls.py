8e3a0f800fb466b57cebe59c977182d055a80e06
João Eugénio <joaoeugenio20@gmail.com>
2026-02-06 08:38:12 +0000

    Update `urls.py` and fix return `__str__` function value

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from books.views import BookCreateListView, BookRetrieveUpdateDestroyView
 from categories.views import CategoryCreateListView, CategoryRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -12,4 +13,7 @@ urlpatterns = [
     
     path('categories/', CategoryCreateListView.as_view(), name='category-create-list'),
     path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail-view'),
+
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
+    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
 ]
