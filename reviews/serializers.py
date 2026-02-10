f8d3973eb318915b28de8d725256d1151d0a254b
João Eugénio <joaoeugenio20@gmail.com>
2026-02-10 15:29:20 +0000

    Config `ReviewSerializer` class to show book title

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,6 +3,11 @@ from reviews.models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    book = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="title"
+    )
+
     class Meta:
         model = Review
         fields = '__all__'
