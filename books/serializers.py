112d8a81ee409e9af41578a936061a59408b1b2d
João Eugénio <joaoeugenio20@gmail.com>
2026-02-10 14:46:46 +0000

    Config `BookSerializer` class to show book categorie name

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from books.models import Book
 
+
 class BookSerializer(serializers.ModelSerializer):
+    categories = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name")
+
     class Meta:
         model = Book
         fields = '__all__'
