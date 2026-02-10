44f5a6e6b895635c465390a5803addb98b2c5179
João Eugénio <joaoeugenio20@gmail.com>
2026-02-10 17:12:47 +0000

    Refactor stars average with `aggregate` & `Avg` methods

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,6 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from books.models import Book
-from reviews.serializers import ReviewSerializer
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -16,16 +16,9 @@ class BookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        reviews = obj.reviews.all()
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
 
-        if reviews:
-            sum_reviews = 0
-
-            for review in reviews:
-                sum_reviews += review.stars
-
-            reviews_count = reviews.count()
-
-            return round(sum_reviews / reviews_count, 1)
+        if rate:
+            return round(rate, 1)
 
         return None
