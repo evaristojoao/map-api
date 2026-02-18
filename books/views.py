593101c521baecf2f507f939511a2c09f73be746
João Eugénio <joaoeugenio20@gmail.com>
2026-02-18 17:10:27 +0000

    Protect all `endpoints` with class `IsAuthenticated`

--- books/views.py
+++ books/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from books.models import Book
 from books.serializers import BookSerializer
 
 
 class BookCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
