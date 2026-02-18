a4d687a9d1e1bcb5e7908b5fd7f41fe2be23ddc7
João Eugénio <joaoeugenio20@gmail.com>
2026-02-18 16:51:06 +0000

    Implement JWT authentication

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('api/v1/', include('authentication.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include('books.urls')),
     path('api/v1/', include('categories.urls')),
