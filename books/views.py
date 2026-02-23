5cbca697385b22c7357a182582354eb7f0165ccf
João Eugénio <joaoeugenio20@gmail.com>
2026-02-23 16:25:23 +0000

    Add permissions global class

--- books/views.py
+++ books/views.py
@@ -1,16 +1,17 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from books.models import Book
 from books.serializers import BookSerializer
 
 
 class BookCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- categories/views.py
+++ categories/views.py
@@ -1,16 +1,16 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from categories.models import Category
 from categories.serializers import CategorySerializer
-from categories.permissions import CategoryPermissionClass
 
 class CategoryCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, CategoryPermissionClass,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, CategoryPermissionClass,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
